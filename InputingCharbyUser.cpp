98fca0d84191a0fbcd61209fa64c43a5d79f0444
Nithin Kumar H S <nithin.s1@s.amity.edu>
2025-02-17 12:26:42 +0530

    Inputing a Character

---
+++ InputingCharbyUser.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Enter a Word :");
+    scanf("%c",&ch);
+    printf("The character entered by the user is :%c",ch);
+    return 0;
+}
+
+
+
