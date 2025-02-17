248a8a8d52ad578acc28e3e8a422963178f4446d
Nithin Kumar H S <nithin.s1@s.amity.edu>
2025-02-17 12:27:56 +0530

    Finding Cube of a number

---
+++ Cubeofanum.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num,x;
+    printf("Enter the number :");
+    scanf("%d",&num);
+    x=pow(num,3);
+    printf("The cube of the number is :%d",x);
+    return 0;
+}
+
