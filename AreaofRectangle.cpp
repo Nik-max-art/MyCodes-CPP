15a4a1ae033f518c59fc9b2718f957d6fee901a3
Nithin Kumar H S <nithin.s1@s.amity.edu>
2025-02-17 12:16:26 +0530

    Area of Rectangle

---
+++ AreaofRectangle.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int l,b,area;
+    printf("Enter the Length of the rectangle :");
+    scanf("%d",&l);
+    printf("Enter the breath of the rectangle :");
+    scanf("%d",&b);
+
+    area=l*b;
+    printf("The area of the rectangle is :%d",area);
+    return 0;
+}
