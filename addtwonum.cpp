2ca5e8ac37d9aae772313b86839ceacb61303ea3
Nithin Kumar H S <nithin.s1@s.amity.edu>
2025-02-17 12:01:38 +0530

    Addition of two numbers

---
+++ addtwonum.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2,sum;
+    printf("ENTER THE NUMBER :");
+    scanf("%d",&num1);
+    printf("ENTER THE NUMBER :");
+    scanf("%d",&num2);
+
+    sum=num1+num2;
+
+    printf("The sum of two numbers is :%d",sum);
+    return 0;
+
+}
+
+
+
